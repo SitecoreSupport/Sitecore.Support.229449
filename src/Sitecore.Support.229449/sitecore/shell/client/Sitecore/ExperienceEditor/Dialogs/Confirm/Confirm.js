07f6c9d1dbb08ecc31ca9694ac556270be826b32
AnnaAkimova <aak@sitecore.net>
2018-07-17 12:57:38 +0300

    Fix commit

--- src/Sitecore.Support.229449/sitecore/shell/client/Sitecore/ExperienceEditor/Dialogs/Confirm/Confirm.js
+++ src/Sitecore.Support.229449/sitecore/shell/client/Sitecore/ExperienceEditor/Dialogs/Confirm/Confirm.js
@@ -3,7 +3,14 @@
         initialized: function () {
             $(this.MessageBody.viewModel.$el[0]).html(decodeURIComponent(ExperienceEditor.Web.getUrlQueryStringValue("message", true)));
             this.setOkButtonClick();
-            this.setCancelButtonClick();          
+            this.setCancelButtonClick();
+            //The fix: change the size of jqueryModalDialogsFrame according to the size of dialogContent
+            setTimeout(function () {
+                var dialogFrame = window.top.document.getElementById("jqueryModalDialogsFrame");
+                var scContentIframeId0Element = dialogFrame.contentWindow.document.getElementById("scContentIframeId0");
+                var dialogContentElement = scContentIframeId0Element.contentWindow.document.getElementById("dialogContent");
+                dialogFrame.contentWindow.setDialogDimension(parseInt(dialogContentElement.clientWidth), parseInt(dialogContentElement.clientHeight + 100));
+            }, 100);
         },
         setCancelButtonClick: function () {
             this.on("button:cancel", function () {
