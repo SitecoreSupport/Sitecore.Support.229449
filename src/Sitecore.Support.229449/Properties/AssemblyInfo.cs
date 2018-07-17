f468f7d60704b935d9120eb57c89f50ca64a6578
AnnaAkimova <aak@sitecore.net>
2018-07-17 12:55:59 +0300

    Init commit

---
+++ src/Sitecore.Support.229449/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.229449")]
+[assembly: AssemblyProduct("Sitecore.Support.229449")]
+[assembly: ComVisible(false)]
